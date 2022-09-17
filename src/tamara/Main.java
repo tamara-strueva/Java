2b3c5921db0619241801f8c51c45d9db2912854d
Тамара <tomka1403200340@gmail.com>
2022-09-17 14:04:55 +0300

    заполнение матрицы учитывает индексы

--- src/tamara/Main.java
+++ src/tamara/Main.java
@@ -1,10 +1,8 @@
 package tamara;
 
 import tamara.homework1.Matrix;
-import tamara.homework1.Vector;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
--- src/tamara/homework1/Matrix.java
+++ src/tamara/homework1/Matrix.java
@@ -1,9 +1,10 @@
 package tamara.homework1;
+import java.util.Arrays;
 
 public class Matrix {
-    private int[][] data = null;
-    private int rows;
-    private int columns;
+    int[][] data = null;
+    int rows;
+    int columns;
 
     Matrix(int rows, int columns){
         this.rows = rows;
@@ -13,20 +14,38 @@ public class Matrix {
 
     public static void main(String[] args){
         Matrix a = new Matrix(3, 4);
-        //a.getMatrix();
         Matrix b = new Matrix(3, 4);
-        a.addMatrix(b);
+        a.setMatrix(new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}});
+        a.getMatrix();
+//        System.out.println(a.toString());
+//        a.addMatrix(b);
     }
 
-    public void setMatrix(){
-        // высчитывать количество строк и столбцов
-        // просить заполнить через консоль или рандом
+    public Matrix setMatrix(int[][] array){
+        Matrix newMatrix = new Matrix(this.rows, this.columns);
+
+        try {
+            for (int row = 0; row < rows; row++) {
+                for (int column = 0; column < columns; column++) {
+                    newMatrix.data[row][column] = array[row][column];
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("!!Введенный массив не соразмерен матрице!!");
+        }
+
+        return newMatrix;
+    }
+
+    public String toString() {
+        return Arrays.deepToString(this.data);
     }
 
     public int getMatrix() {
         for(int row = 0; row < rows; row++){
             for(int column = 0; column < columns; column++){
-                System.out.print(data[row][column] + " ");
+                System.out.print(data[row][column]);
+                System.out.print("\t");
             }
             System.out.println();
         }
@@ -62,3 +81,26 @@ public class Matrix {
     }
     */
 }
+
+//import java.util.Arrays;
+//
+//public class Matrix {
+//
+//    int[][] matrix;
+//
+//    public Matrix(int[][] matrix){
+//        this.matrix = matrix;
+//    }
+//
+//    public String toString(){
+//        return Arrays.deepToString(matrix);
+//    }
+//
+//    public Matrix sumMatrix(Matrix matrixA, Matrix matrixB){
+//        Matrix result = new Matrix(new int[matrix.length][matrix.length]);
+//        for (int i = 0; i < matrixA.matrix.length; i++)
+//            for (int j = 0; j < matrixA.matrix.length; j++)
+//                result.matrix[i][j] = matrixA.matrix[i][j] + matrixB.matrix[i][j];
+//        return result;
+//    }
+//}
