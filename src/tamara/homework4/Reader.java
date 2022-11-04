7483188bb0576b4e7b7f34f6da51bf88fc5df761
Тамара <tomka1403200340@gmail.com>
2022-11-04 16:49:28 +0300

    доделано дз, дописаны комментарии

--- src/tamara/homework4/Reader.java
+++ src/tamara/homework4/Reader.java
@@ -1,11 +1,11 @@
 package tamara.homework4;
-import java.util.*;
 import java.lang.*;
 
 public class Reader {
     String FIO, faculty, phoneNumber, dateOfBirth;
     int idOfReaderTicket;
 
+    // конструктор класса
     Reader (String FIO, int idOfReaderTicket, String faculty, String phoneNumber, String dateOfBirth) {
         this.FIO = FIO;
         this.idOfReaderTicket = idOfReaderTicket;
@@ -13,10 +13,12 @@ public class Reader {
         this.phoneNumber = phoneNumber;
     }
 
+    // метод, выводящий, сколько книг взял читатель
     public void takeBook(int numberOfTakenBooks) {
         System.out.println(this.FIO + " взял " + numberOfTakenBooks + " книги");
     }
 
+    // метод, выводящий названия книг, которые взял читатель
     public void takeBook(String... namesOfTakenBooks) {
         System.out.print("\n" + this.FIO + " взял книги: ");
         for (String book: namesOfTakenBooks) {
@@ -24,6 +26,7 @@ public class Reader {
         }
     }
 
+    // метод, выводящий названия книг, которые взял читатель
     public void takeBook(Book... books) {
         System.out.print("\n" + this.FIO + " взял книги: ");
         for (Book book: books) {
@@ -32,10 +35,12 @@ public class Reader {
         System.out.println();
     }
 
+    // метод, выводящий количество книг, которые вернул читатель
     public void returnBook(int numberOfReturnedBooks) {
         System.out.println(this.FIO + " вернул " + numberOfReturnedBooks + " книг");
     }
 
+    // метод, выводящий названия книг, которые вернул читатель
     public void returnBook (String... namesOfReturnedBooks) {
         System.out.println("\n" + this.FIO + " вернул книги: ");
         for (String book: namesOfReturnedBooks) {
@@ -44,6 +49,7 @@ public class Reader {
         System.out.println();
     }
 
+    // метод, выводящий названия книг, которые вернул читатель
     public void returnBook(Book... books) {
         System.out.print("\n" + this.FIO + " вернул книги: ");
         for (Book book: books) {
@@ -52,13 +58,18 @@ public class Reader {
         System.out.println();
     }
 
+    // метод, возвращающий ФИО читателя
     public String getFIO() {return this.FIO;}
 
+    // метод, возвращающий номер читательского билета
     public int getIdOfReaderTicket() {return this.idOfReaderTicket;}
 
+    // метод, возвращающий факультет, на котором читатель учится
     public String getFaculty() {return this.faculty;}
 
+    // метод, возращающий телефонный номер читателя
     public String getPhoneNumber() {return this.phoneNumber;}
 
+    // метод, возвращающий дату рождения читателя
     public String getDateOfBirth() {return this.dateOfBirth;}
 }
--- src/tamara/homework4/Seasons.java
+++ src/tamara/homework4/Seasons.java
@@ -1,31 +1,48 @@
 package tamara.homework4;
-import java.util.*;
 import java.lang.*;
+import java.util.Scanner;
 
 public enum Seasons {
+    //пречисление времен года и средней температуры
     WINTER(-17),
     SPRING(9),
     AUTUMN(6),
     SUMMER(20){
+        // вывод, какое время года, потому что только лето теплое
         public void getDescription() {
             System.out.println("Теплое время года");
         }
     };
     double averageTemperature;
-//    final String favouriteSeason = "SUMMER";
+    final String favouriteSeason = "SUMMER";
 
+    // список сезонов, чтобы вывести описание
     String[] seasons = new String[] {"WINTER", "SPRING", "SUMMER", "AUTUMN"};
 
     public static void main(String[] args) {
+        // вывод информации о временах года
         for (Seasons season : Seasons.values()) {
-            season.printAboutSeason(season);
+            printAboutSeason(season);
+        }
+
+        // запрос ввести любимое время года, чтобы вывести о нем информацию
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите свое любимое время года: ");
+        String favouriteSeason = scan.nextLine();
+
+        for (Seasons season : Seasons.values()) {
+            if(season.toString().compareTo(favouriteSeason) == 0) {
+                printAboutSeason(season);
+            }
         }
     }
 
+    // конструктор, в котором указывается средняя температура сезона
     Seasons(double averageTemperature){
         this.averageTemperature = averageTemperature;
     }
 
+    // вывод сообщения "я люблю..."
     public void iLove(Seasons season) {
         switch (season) {
             case WINTER -> System.out.println("Я люблю зиму!");
@@ -35,19 +52,23 @@ public enum Seasons {
         }
     }
 
+    // вывод информации о времени года
     public static void printAboutSeason(Seasons season){
         System.out.println("Average temperature in " + season + ": " + season.getAverageTemperature() + "\u00B0C");
         season.getDescription();
     }
 
+    // вывод описания времени года
     public void getDescription() {
         System.out.println("Холодное время года");
     }
 
+    // метод, возвращающий среднюю температуру времени года
     public double getAverageTemperature (){
         return averageTemperature;
     }
 
+    // метод, чтобы задать среднюю температуру
     public void setAverageTemperature(double averageTemperature) {
         this.averageTemperature = averageTemperature;
     }
